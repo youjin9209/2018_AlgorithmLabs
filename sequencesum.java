465ea55f3d8ac9cd0e24798a0aa22f61eba6bdb0
scarlett <youjin9200@naver.com>
2018-07-23 11:35:08 +0900

    Update sequencesum.java

--- sequencesum.java
+++ sequencesum.java
@@ -5,28 +5,28 @@ public class sequencesum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[][] a = new int[N+2][N+2];
-		int totalSum = 0;
+		long[][] a = new long[N+2][N+2];
+		long totalSum = 0;
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
 				a[i][j] = sc.nextInt();
-				totalSum += a[i][j];
+				if (i != j)
+					totalSum += a[i][j];
 			}
 		}
 		
-		totalSum /= 6;
-		int[] d = new int[N+1];
+		totalSum /= (2*N-2);
+		long[] d = new long[N+1];
 		for (int i = 1; i <= N; i++) {
-			int rowSum = 0;
+			long rowSum = 0;
 			for (int j = 1; j <= N; j++) {
 				if (i != j)
 					rowSum += a[i][j];
 			}
-			d[i] = (rowSum - totalSum) / 2;
+			d[i] = (rowSum - totalSum) / (N-2);
 		}
 		for (int i = 1; i <= N; i++) {
 			System.out.print(d[i] + " ");
 		}
 	}
-
 }
