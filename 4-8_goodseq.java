1010c5b9d5391d2391f2bd8b1c1d78dd793345b7
scarlett <youjin9200@naver.com>
2018-08-09 01:14:53 +0900

    Update 4-8_goodseq.java

--- 4-8_goodseq.java
+++ 4-8_goodseq.java
@@ -1,62 +1,65 @@
 /*
-최적화 시키기 ! 
-*/
-import java.util.Scanner;
+<문제>
+숫자 1, 2, 3으로만 이루어지는 수열이 있다. 임의의 길이의 인접한 두 개의 부분 수열이 동일한 것 이 있으면, 그 수열을 나쁜 수열이라고 부른다. 
+그렇지 않은 수열은 좋은 수열이다.
+다음은 나쁜 수열의 예이다.
+33
+32121323
+123123213
+다음은 좋은 수열의 예이다.
+2
+32
+32123
+1232123
+길이가 N인 좋은 수열들을 N자리의 정수로 보아 그중 가장 작은 수를 나타내는 수열을 구하는 프로그램 을 작성하라. 
+예를 들면, 1213121과 2123212는 모두 좋은 수열이지만 그 중에서 작은 수를 나타내는 수 열 1213121이다.
+ 
+<입력>
+입력은 숫자 N하나로 이루어진다. N은 1 이상 80 이하이다.
+<출력>
+첫 번째 줄에 1, 2, 3으로만 이루어져 있는 길이가 N인 좋은 수열들 중에서 가장 작은 수를 나타내 는 수열만 출력한다. 
+수열을 이루는 1, 2, 3들 사이에는 빈칸을 두지 않는다.
 
-public class goodseq {
-	static int[] arr;
-	static int[] num;
-	static int min = 4;
+<예제 입력>
+7
+<예제 출력>
+1213121
+*/
+package algorithm_main;
+import java.util.*;
+
+public class Main {
 	static int N;
-	public static void print() {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			sb.append(arr[i]);
-		}
-		System.out.println(sb);
+	
+	public static boolean check (String s) {
+	    int len = s.length();
+	    int start = len-1;
+	    int end = len;
+	    for (int width = 1; width <= len/2; width++) { // width : 비교해줄 문자열의 덩어리 길이 
+	    	String left = s.substring(start-width, end-width);
+	    	String right = s.substring(start, end);
+	        if (left.equals(right)) {
+	            return false;
+	        }
+	        start -= 1; //width가 1씩 늘어나니까 오른쪽 덩어리 시작점을 하나씩 왼쪽으로 댕겨야지 ! 
+	    }
+	    return true;
 	}
-	public static boolean check(int index) { 
-		if (index <= 20 || N <= 3) {
-			for (int i = 1; i <= index; i++) {
-				if (arr[i] == arr[i-1])
-					return false;
+	public static void goodseq (int length, String str) {
+	    if (length == N) {
+			System.out.println(str);
+			System.exit(0);
+	    } else {
+			for (int i = 1; i <= 3; i++) { // 1,2,3 넣기 - 완전탐색 : 이전 거에 붙여주면서 ! 
+			    if (check(str+i)) {
+			    	goodseq(length+1, str+i);
+			    }
 			}
-		}
-		if (N >= 4) { // 자리수 1 ~ N/2 인접안하는지 확인 
-			for (int length = 1; length <= (N/2); length++) { // 덩어리 길이 
-				for (int i = 0; i <= N - 2*length; i++) { // 시작점 : 가장 큰 i 를 x 라고 했을 때  ( x + length + length - 1 = N - 1 )
-					int flag = 0;
-					for (int j = i; j <= i + length-1; j++) { // 비교
-						if (arr[j] == arr[j+length]) {
-							flag++;
-						}
-					}
-					if (flag == length)
-						return false;
-				}
-			}
-		}
-		return true;
+	    }
 	}
-	public static void goodseq(int index) {
-		if (index > N-1) {
-			if (check(index-1)) {
-				print();
-				System.exit(0);
-        	}
-            return;
-        }
-		for (int i = 0; i < 3; i++) { // 1,2,3 순서대로 하니까 먼저 check 에서 통과됐으면 break  
-        	arr[index] = num[i];
-        	goodseq(index+1);
-		}
-		
-	}
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		arr = new int[N];
-		num = new int[]{1,2,3};
-		goodseq(0);
+		goodseq(1, "1");
 	}
 }
